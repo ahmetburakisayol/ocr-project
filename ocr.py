00aa44dfdd298b3ce0bcb93c46e0e66c09f2ce5f
Ahmet Burak Kısayol <86475964+ahmetburakisayol@users.noreply.github.com>
2025-10-28 14:33:35 +0300

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -5,5 +5,6 @@ import pytesseract ##
 sys.stdout.reconfigure(encoding='utf-8') ## BURADA ÇIKTI VERİRKEN TÜRKÇE KARAKTERLERİ BOZUK ÇIKARTMASIN DİYE UTF-8'E ÇEVİRİYORUZ.
 
 
-hadibakalim = pytesseract.image_to_string(Image.open("ocr-dosya.png"), lang="tur") ## yazdırma kısımı
-print(hadibakalim) ### maalesef çok beceriksizsin başak :/
+ocr_yap = pytesseract.image_to_string(Image.open("ocr-dosya.png"), lang="tur") ## yazdırma kısımı
+print(ocr_yap) #yazdirma kısımı
+
